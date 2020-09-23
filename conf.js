98a0a51f4f5999255d05f967bf4b706519103a32
Papabyte <bcreze@protonmail.com>
2020-09-23 18:35:59 +0700

    new aas

--- conf.js
+++ conf.js
@@ -9,10 +9,9 @@ exports.apiPort = process.env.testnet ? 4001 : 4000;
 
 exports.hub = process.env.testnet ? 'obyte.org/bb-test' : 'obyte.org/bb';
 exports.explorer_base_url = process.env.testnet ? 'https://testnetexplorer.obyte.org/#' : 'https://explorer.obyte.org/#';
-exports.stablecoins_base_url = process.env.testnet ? 'https://stable.twoogi.ru/trade/' : 'https://explorer.obyte.org/#';
 
-exports.curve_base_aa = process.env.testnet ? "ZQENBYPGFZM2DYFM3OJAONMXC7UOVD4W" : "ZQENBYPGFZM2DYFM3OJAONMXC7UOVD4W";
-exports.deposit_base_aa = process.env.testnet ? "X7FWWFNENQP5VN77JTAZQ7DTCHZEYXEQ" : "X7FWWFNENQP5VN77JTAZQ7DTCHZEYXEQ";
+exports.curve_base_aa = process.env.testnet ? "FCFYMFIOGS363RLDLEWIDBIIBU7M7BHP" : "FCFYMFIOGS363RLDLEWIDBIIBU7M7BHP";
+exports.deposit_base_aa = process.env.testnet ? "GEZGVY4T3LK6N4NJAKNHNQIVAI5OYHPC" : "GEZGVY4T3LK6N4NJAKNHNQIVAI5OYHPCß";
 exports.token_registry_aa_address = process.env.testnet ? "O6H6ZIFI57X3PLTYHOCVYPP5A553CYFQ" : "O6H6ZIFI57X3PLTYHOCVYPP5A553CYFQ";
 
 console.log('finished server conf');
