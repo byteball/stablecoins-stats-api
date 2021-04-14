38720f7545a5785a52cbb60676e2d2fdea20a8d4
tony <tonyofbyteball@users.noreply.github.com>
2021-04-14 18:48:57 +0300

    same AAs for testnet

--- conf.js
+++ conf.js
@@ -12,14 +12,11 @@ exports.apiPort = process.env.testnet ? 4001 : 4000;
 exports.hub = process.env.testnet ? 'obyte.org/bb-test' : 'obyte.org/bb';
 exports.explorer_base_url = process.env.testnet ? 'https://testnetexplorer.obyte.org/#' : 'https://explorer.obyte.org/#';
 
-exports.curve_base_aas = process.env.testnet ? 
-["FCFYMFIOGS363RLDLEWIDBIIBU7M7BHP", "3RNNDX57C36E76JLG2KAQSIASAYVGAYG", "STA7KVV77IHZ3S34YAFZAIKUGPIK4EZ7"]
-: 
-["FCFYMFIOGS363RLDLEWIDBIIBU7M7BHP", "3RNNDX57C36E76JLG2KAQSIASAYVGAYG", "STA7KVV77IHZ3S34YAFZAIKUGPIK4EZ7"];
-exports.deposit_base_aa = process.env.testnet ? "GEZGVY4T3LK6N4NJAKNHNQIVAI5OYHPC" : "GEZGVY4T3LK6N4NJAKNHNQIVAI5OYHPC";
+exports.curve_base_aas = ["FCFYMFIOGS363RLDLEWIDBIIBU7M7BHP", "3RNNDX57C36E76JLG2KAQSIASAYVGAYG", "STA7KVV77IHZ3S34YAFZAIKUGPIK4EZ7"];
+exports.deposit_base_aa = "GEZGVY4T3LK6N4NJAKNHNQIVAI5OYHPC";
 exports.stable_base_aa = "YXPLX6Q3HBBSH2K5HLYM45W7P7HFSEIN";
 exports.fund_base_aa = "5WOTEURNL2XGGKD2FGM5HEES4NKVCBCR";
 
-exports.token_registry_aa_address = process.env.testnet ? "O6H6ZIFI57X3PLTYHOCVYPP5A553CYFQ" : "O6H6ZIFI57X3PLTYHOCVYPP5A553CYFQ";
+exports.token_registry_aa_address = "O6H6ZIFI57X3PLTYHOCVYPP5A553CYFQ";
 
 console.log('finished server conf');
